5b0c50af26dbd070a0ba340e8df2f174a565bc22
asimsek17 <simsekab18@itu.edu.tr>
2022-09-27 23:37:55 +0300

    password validation added

--- models/login_input.go
+++ models/login_input.go
@@ -7,12 +7,11 @@ import (
 )
 
 var EmailDomainCheck validator.Func = func(fl validator.FieldLevel) bool {
-	emailhost := "@itu.edu.tr"
-	return strings.Contains(fl.Field().String(), emailhost)
+	return strings.Contains(fl.Field().String(), "@itu.edu.tr")
 
 }
 
 type LoginInput struct {
 	Mail     string `json:"mail" binding:"required,email,EmailDomainCheck"`
-	Password string `json:"password" binding:"required"`
+	Password string `json:"password" binding:"required,min=8,max=20"`
 }
