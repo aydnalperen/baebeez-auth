cfc506c2d6921c0240d4c3a307f6557b67ee5e45
asimsek17 <simsekab18@itu.edu.tr>
2022-09-27 23:12:35 +0300

    custom emailvalidation added

--- models/login_input.go
+++ models/login_input.go
@@ -1,6 +1,18 @@
 package models
 
+import (
+	"strings"
+
+	"github.com/go-playground/validator/v10"
+)
+
+var EmailDomainCheck validator.Func = func(fl validator.FieldLevel) bool {
+	emailhost := "@itu.edu.tr"
+	return strings.Contains(fl.Field().String(), emailhost)
+
+}
+
 type LoginInput struct {
-	Mail     string `json:"mail" binding:"required"`
+	Mail     string `json:"mail" binding:"required,email,EmailDomainCheck"`
 	Password string `json:"password" binding:"required"`
 }
